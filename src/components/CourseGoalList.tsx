e9d25a00a663340c8519428094a5c9072e4b5ea5
Revoj-a <revojjoever@gmail.com>
2025-12-17 15:25:12 +0800

    Build InfoBox component for message

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal";
 import { CourseGoal as CGoal } from "../App";
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 interface CourseGoal {
   goals: CGoal[];
@@ -7,16 +9,35 @@ interface CourseGoal {
 }
 
 const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoal) => {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course gaols yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+
+interface InfoBoxProps {
+  mode: "hint" | "warning";
+  severity?: "low" | "medium" | "high";
+  children: ReactNode;
+}
+
+const InfoBox = ({ mode, children, severity }: InfoBoxProps) => {
+  if (mode === "hint") {
+    return (
+      <aside className="infobox infobox-hint">
+        <p>{children}</p>
+      </aside>
+    );
+  }
+  return (
+    <aside className={`infobox infobox-warning warning--${severity}`}>
+      <h2>Warning</h2>
+      <p>{children}</p>
+    </aside>
+  );
+};
+
+export default InfoBox;
